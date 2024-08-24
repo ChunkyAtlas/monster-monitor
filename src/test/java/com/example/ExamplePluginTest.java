5eb251891d73384417e5b774b8ebcb2e69f05934
ChunkyAtlas <justinsmith4621@proton.me>
2024-08-24 17:51:46 -0400

    added plugin

--- src/test/java/com/example/ExamplePluginTest.java
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.monstermonitor.MonsterMonitorPlugin;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
@@ -7,7 +8,7 @@ public class ExamplePluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(MonsterMonitorPlugin.class);
 		RuneLite.main(args);
 	}
 }
